45e30f4d68efd0baec49de25a64a4e9d68e6bc14
zylo117 <zylo117@hotmail.com>
2018-05-18 16:05:15 +0800

    add Backpropagation

---
+++ starter/nn/nn_mnist.py
@@ -0,0 +1,32 @@
+from starter.nn.neuralnetwork import NeuralNetwork
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn import datasets
+
+# load the MNIST dataset and apply min/max scaling to scale the
+# pixel intensity values to the range [0, 1] (each image is
+# represented by an 8 x 8 = 64-dim feature vector)
+print("[INFO] loading MNIST (sample) dataset...")
+digits = datasets.load_digits()
+data = digits.data.astype("float")
+data = (data - data.min()) / (data.max() - data.min())
+print("[INFO] samples: %d, dim: %d" % (data.shape[0], data.shape[1]))
+
+# construct the training and testing splits
+(trainX, testX, trainY, testY) = train_test_split(data, digits.target, test_size=0.25)
+
+# construct the training and testing splits
+trainY = LabelBinarizer().fit_transform(trainY)
+testY = LabelBinarizer().fit_transform(testY)
+
+# train the network
+print("[INFO] training network...")
+nn = NeuralNetwork([trainX.shape[1], 32, 16, 10])
+print("[INFO] %s" % nn)
+nn.fit(trainX, trainY, epochs=1000)
+
+print("[INFO] evaluating network...")
+predictions = nn.predict(testX)
+predictions = predictions.argmax(axis=1)
+print(classification_report(testY.argmax(axis=1), predictions))
---
+++ starter/nn/nn_xor.py
@@ -0,0 +1,17 @@
+from starter.nn.neuralnetwork import NeuralNetwork
+import numpy as np
+
+X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+y = np.array([[0], [1], [1], [0]])
+
+# define our 2-2-1 neural network and train it
+nn = NeuralNetwork([2, 2, 1], alpha=0.5)
+nn.fit(X, y, epochs=20000)
+
+# now that our network is trained, loop over the XOR data points
+for (x, target) in zip(X, y):
+    # make a prediction on the data point and display the result
+    # to our console
+    pred = nn.predict(x)[0][0]
+    step = 1 if pred > 0.5 else 0
+    print("[INFO] data=%s, ground-truth=%s, pred=%.7f, step=%d" % (x, target[0], pred, step))
