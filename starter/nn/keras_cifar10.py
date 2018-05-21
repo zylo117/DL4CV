577f2563a3fefc1fedc38693aac880a5db609fb8
zylo117 <zylo117@hotmail.com>
2018-05-21 13:55:31 +0800

    add solution for tensorflow-gpu on mac

--- starter/nn/keras_cifar10.py
+++ starter/nn/keras_cifar10.py
@@ -15,7 +15,7 @@ args = vars(ap.parse_args())
 # load the training and testing data, scale it into the range [0, 1],
 # then reshape the design matrix
 print("[INFO] loading CIFAR-10 data...")
-((trainX, trainY),(testX, testY)) = cifar10.load_data()
+((trainX, trainY), (testX, testY)) = cifar10.load_data()
 trainX = trainX.astype("float") / 255
 testX = testX.astype("float") / 255
 trainX = trainX.reshape((trainX.shape[0], 3072))
@@ -32,3 +32,30 @@ labelNames = ["airplane", "automobile", "bird", "cat", "deer", "dog", "frog", "h
 # define the 3072-1024-512-10 architecture using Keras
 model = Sequential()
 model.add(Dense(1024, input_shape=(3072,), activation="relu"))
+model.add(Dense(512, activation="relu"))
+model.add(Dense(10, activation="softmax"))
+
+# train the model using SGD
+print("[INFO] training network...")
+sgd = SGD(0.01)
+model.compile(loss="categorical_crossentropy", optimizer=sgd, metrics=["accuracy"])
+H = model.fit(trainX, trainY, validation_data=(testX, testY), epochs=100, batch_size=32)
+
+# evaluate the network
+print("[INFO] evaluating network...")
+predictions = model.predict(testX, batch_size=32)
+print(classification_report(testY.argmax(axis=1), predictions.argmax(axis=1), target_names=labelNames))
+
+# plot the training loss and accuracy
+plt.style.use("ggplot")
+plt.figure()
+plt.plot(np.arange(100), H.history["loss"], label="val_loss")
+plt.plot(np.arange(100), H.history["val_loss"], label="val_loss")
+plt.plot(np.arange(100), H.history["acc"], label="train_acc")
+plt.plot(np.arange(100), H.history["val_acc"], label="val_acc")
+plt.title("Training Loss and Accuracy")
+plt.xlabel("Epoch #")
+plt.ylabel("Loss/Accuracy")
+plt.legend()
+plt.savefig(args["output"])
+plt.show()
