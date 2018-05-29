39af69b3ccb4a22a34a422bc050a43cecff07b30
zylo117 <zylo117@hotmail.com>
2018-05-29 20:48:44 +0800

    update mini VGG Net

--- starter/cnn/nn/conv/lenet.py
+++ starter/cnn/nn/conv/lenet.py
@@ -22,3 +22,20 @@ class LeNet:
         model.add(Conv2D(20, (5, 5), padding="same", input_shape=inputShape))
         model.add(Activation("relu"))
         model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+        # second set of CONV => RELU => POOL layers
+        model.add(Conv2D(50, (5, 5), padding="same"))
+        model.add(Activation("relu"))
+        model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+        # first (and only) set of FC => RELU layers
+        model.add(Flatten())
+        model.add(Dense(500))
+        model.add(Activation("relu"))
+
+        # softmax classifier
+        model.add(Dense(classes))
+        model.add(Activation("softmax"))
+
+        # return the constructed network architecture
+        return model
